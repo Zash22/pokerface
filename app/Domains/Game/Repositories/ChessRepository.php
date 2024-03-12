eb7c5f34ee624e7ffa284fd538d725be8510562e
zash <zash22@gmail.com>
2024-03-12 10:10:57 +0200

    Game repos

---
+++ app/Domains/Game/Repositories/ChessRepository.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Domains\Game\Repositories;
+class ChessRepository
+{
+    public function evaluate(array $data): string
+    {
+        return 'Chess';
+    }
+}
---
+++ app/Domains/Game/Repositories/PokerfaceRepository.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Domains\Game\Repositories;
+
+class PokerfaceRepository
+{
+    public function evaluate(array $data): string
+    {
+        return 'PokerFace';
+    }
+}
